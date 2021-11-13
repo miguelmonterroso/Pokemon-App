c169fb699f791dcd3655634b8c9d15130e379bd5
Miguel Monterroso <miguelmonterroso39@gmail.com>
2021-11-13 11:37:41 -0600

    se empezo a trabjar en la carta y se empezaron a solucionar algunos problemas que estaba causando material UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import { CustomCard } from "./components/cards/card";
+import { makeStyles } from '@mui/styles'
 
+
+const useStyles = makeStyles({
+  root:{
+      background:'#F1FAEE',
+      color:'white',
+      height:'100vh',
+      width:'100vw',
+      marginLeft:'-8px',
+      marginTop:'-21px',
+      padding:0,
+      boxSizing:'border-box'
+  }
+})
 
 function App() {
+  const classes = useStyles()
   return (
-    <div>
+    <div className={classes.root}>
       <CustomCard/>
     </div>
   );
--- src/components/cards/card.jsx
+++ src/components/cards/card.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { makeStyles } from '@mui/styles'
+
+
+const useStyles = makeStyles({
+    root:{
+        color:'#1D3557'
+    }
+})
 
 export const CustomCard = () => {
+    const classes = useStyles()
     return (
-        <div>
+        <div className={classes.root}>
             <h1>Hello from my card</h1>
         </div>
     )
