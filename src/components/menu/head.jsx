dbc3973ffc87b56706ddafe5511235297a25260e
Miguel Monterroso <miguelmonterroso39@gmail.com>
2021-11-13 15:02:20 -0600

    se agregaron los estilos al menu y se agrego el icono, se empieza a ver la funciones

--- src/components/menu/head.jsx
+++ src/components/menu/head.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import logo from '../../images/logo.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 export const Head = () => {
     return (
         <div className='container'>
             <img className='logo' src={logo} alt="logo" />
-            <FontAwesomeIcon/>
+            <FontAwesomeIcon className='icon' icon={faBars}/>
         </div>
     )
 }
