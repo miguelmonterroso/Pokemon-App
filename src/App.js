7944635f802cdecf5a76ccc5ff01262233e4b2d8
Miguel Monterroso <miguelmonterroso39@gmail.com>
2021-11-13 14:24:14 -0600

    se hizo un reset de css en la app y se empezo a trabajar en el header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { CustomCard } from "./components/cards/card";
+import { Head } from "./components/menu/head";
 import './styles/main.css'
 
 function App() {
   return (
     <div className='home'>
+      <Head/>
       <CustomCard/>
     </div>
   );
---
+++ src/components/menu/head.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Head = () => {
+    return (
+        <div className='container'>
+            <h1 className='menu'>soy la cabeza</h1>
+            
+        </div>
+    )
+}
