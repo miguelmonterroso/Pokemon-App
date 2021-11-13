e421fbf74fecf7405d587673806365ca5eaa6ac5
Miguel Monterroso <miguelmonterroso39@gmail.com>
2021-11-13 14:12:03 -0600

    se hicieron cambios y se empezo a trabajar con SASS ya que material ui estaba dando problemas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
 import { CustomCard } from "./components/cards/card";
-import { makeStyles } from '@mui/styles'
-
-
-const useStyles = makeStyles({
-  root:{
-      background:'#F1FAEE',
-      color:'white',
-      height:'100vh',
-      width:'100vw',
-      marginLeft:'-8px',
-      marginTop:'-21px',
-      padding:0,
-      boxSizing:'border-box'
-  }
-})
+import './styles/main.css'
 
 function App() {
-  const classes = useStyles()
   return (
-    <div className={classes.root}>
+    <div className='home'>
       <CustomCard/>
     </div>
   );
--- src/components/cards/card.jsx
+++ src/components/cards/card.jsx
@@ -1,17 +1,8 @@
 import React from 'react'
-import { makeStyles } from '@mui/styles'
-
-
-const useStyles = makeStyles({
-    root:{
-        color:'#1D3557'
-    }
-})
 
 export const CustomCard = () => {
-    const classes = useStyles()
     return (
-        <div className={classes.root}>
+        <div>
             <h1>Hello from my card</h1>
         </div>
     )
