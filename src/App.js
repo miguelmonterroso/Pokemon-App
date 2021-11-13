16ad0b9a27277872eaaee5043599f12539859331
Miguel Monterroso <miguelmonterroso39@gmail.com>
2021-11-13 11:00:17 -0600

    prueba

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
       <p>
-          Hola
+          adios
         </p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
