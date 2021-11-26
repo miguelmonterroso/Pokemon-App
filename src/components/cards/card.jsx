7d06373f8b9ba105c4d284aefb0f84a3bae00f6e
Miguel Monterroso <miguelmonterroso39@gmail.com>
2021-11-25 19:04:24 -0600

    se continua la documentacion de los componentes card y la pagina home

--- src/components/cards/card.jsx
+++ src/components/cards/card.jsx
@@ -1,6 +1,12 @@
+// Los estilos de este componente se pueden modificar en el archivo _card.scss
 import React from 'react'
 
+// Exportamos nuestra funcion CustomCard que recibe las props de la informacion que se
+// desea renderizar
 export const CustomCard = (props) => {
+    // retornamos lo que va a ser visualizado en pantalla
+    // y accedemos a las props que nos enviara el componente getPokemos
+    // este componente esta diseÑado para ser dinamico
     return (
         <div className='cardContainer'>
             <div className='center'>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,11 @@
+// Los estilos de este componente se puden modificar en el archivo _home.scss
 import React from 'react'
+// Importamos nuestro componente GetPokemos para renderizarlo
 import { GetPokemons } from '../components/cards/getPokemos'
 
+// Exportamos la funcion Homa para poder renderizarla en la app y agregarla a nuestro Router
 export const Home = () => {
+    // Retornamos nuestro componente para renderizar cuando se ingrese a la url
     return (
         <div className='render'>
             <GetPokemons/>
